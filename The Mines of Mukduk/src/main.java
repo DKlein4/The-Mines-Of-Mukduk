765b064b8d8c156e0783cbd14a45da78e98129d2
Dustin <Dustin@Dustins-Laptop>
2017-09-17 01:00:14 -0500

    Trying something out

--- The Mines of Mukduk/src/main.java
+++ The Mines of Mukduk/src/main.java
@@ -4,7 +4,6 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Adding something more
-		// This is a new comment
+		// Dustin is awesome
 	}
-
 }
