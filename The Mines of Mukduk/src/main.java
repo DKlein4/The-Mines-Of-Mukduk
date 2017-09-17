3838c742dddf892f0d8cab0075d7744c0aeb2c07
Brett <Brett@Bretts-PC.home>
2017-09-17 02:27:07 -0500

    Framework of mainMenu function in UI class. Tried to get a clear console class working but did not work so deleted it.

--- The Mines of Mukduk/src/main.java
+++ The Mines of Mukduk/src/main.java
@@ -1,7 +1,8 @@
+import user_interface.UI;
 
 public class main {
-
 	public static void main(String[] args) {
+		UI.mainMenu();
 		// TODO Auto-generated method stub
 	}
 	
--- The Mines of Mukduk/src/user_interface/UI.java
+++ The Mines of Mukduk/src/user_interface/UI.java
@@ -1,5 +1,34 @@
 package user_interface;
-
+import java.util.Scanner;
 public class UI {
 
+	public static void mainMenu() {
+		Scanner scan = new Scanner(System.in);
+		int playerChoice = 0;
+		
+		System.out.println("1: Play Game");
+		System.out.println("2: How to Play");
+		System.out.println("3: Credits");
+		System.out.print("Choice: ");
+		playerChoice = scan.nextInt();
+		
+		if (playerChoice == 1) {
+			//startGame Function		
+		}
+		else if (playerChoice == 2) {
+			helpScreen();
+		}
+		else if (playerChoice == 3) {
+			//credits Function
+		}
+		else {
+			System.out.println("Please enter valid function.");
+			scan.nextLine();
+			mainMenu();
+		}
+	}
+	
+	public static void helpScreen() {
+		System.out.println("This is how to play the game!");
+	}
 }
