d71c174142fa782aad8800d66493689cf441c521
Brett <Brett@Bretts-PC.home>
2017-09-17 01:32:15 -0500

    Merged?

--- The Mines of Mukduk/src/main.java
+++ The Mines of Mukduk/src/main.java
@@ -3,12 +3,6 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		// Adding something more
-		// Dustin is not awesome
-		// Test
-=======
->>>>>>> branch 'master' of https://github.com/DKlein4/The-Mines-Of-Mukduk
 	}
 	
 	static void doesNothing(){
@@ -18,6 +12,4 @@ public class main {
 	static void somethingImportant(){
 		//does something important
 	}
-	
-	// another thing to update
 }
