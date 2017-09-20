c7817c1a7f1218657c2077fd89f10c97a4e9c44c
Dustin <Dustin@Dustins-Laptop>
2017-09-20 15:09:24 -0500

    Fixed the issue were x and y were flipped.

--- The Mines of Mukduk/src/main.java
+++ The Mines of Mukduk/src/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// UI userInterface = new UI();
 		// userInterface.mainMenu();
-
+		
 		Map map = new Map(30);
 		map.printGrid();
 
--- The Mines of Mukduk/src/world_gen/Map.java
+++ The Mines of Mukduk/src/world_gen/Map.java
@@ -71,35 +71,32 @@ public class Map {
 			int[] room2 = genRoom();
 
 			// So it turns out width is up and down and height is left to right...oops
-			System.out.println("Room 1: (" + room1[2] + "," + room1[3] + ") to (" + room1[4] + "," + room1[5] + ") w:"
-					+ room1[0] + " h:" + room1[1]);
-			System.out.println("Room 2: (" + room2[2] + "," + room2[3] + ") to (" + room2[4] + "," + room2[5] + ") w:"
-					+ room2[0] + " h:" + room2[1]);
+			System.out.println("Room " + (2*i) + ": \t(" + room1[2] + "," + room1[3] + ")\tto \t(" + room1[4] + "," + room1[5] + ")   \tw:" + room1[0] + " h:" + room1[1]);
+			System.out.println("Room " + (2*i + 1) + ": \t(" + room2[2] + "," + room2[3] + ")\tto \t(" + room2[4] + "," + room2[5] + ")   \tw:" + room2[0] + " h:" + room2[1]);
 
 			// Create the corridors between them
-			int x1Center = (room1[2] + room1[4]) / 2;
-			int x2Center = (room2[2] + room2[4]) / 2;
-			int y1Center = (room1[3] + room1[5]) / 2;
-			int y2Center = (room2[3] + room2[5]) / 2;
-
+			int x1Center = (room1[2] + room1[4]) / 2;	// First room's center x
+			int x2Center = (room2[2] + room2[4]) / 2;	// Second room's center x
+			int y1Center = (room1[3] + room1[5]) / 2;	// First room's center y
+			int y2Center = (room2[3] + room2[5]) / 2;	// Second room's center x
+			
+			// Use those values as the coordinates for the corridors to make them match up to the center of the rooms.
 			genXCorridor(x1Center, x2Center, y2Center);
 			genYCorridor(y1Center, y2Center, x1Center);
 		}
 	}
 
 	private int[] genRoom() {
-		/*
-		 * w - width of the rect 
-		 * h - height of the rect 
-		 * xo - x coordinate of the origin of the rect 
-		 * yo - y coordinate of the origin of the rect 
-		 * xc - x coordinate of the corner opposite of the origin 
-		 * yc - y coordinate of the corner opposite of the origin
-		 */
+		// w - width of the rect
+		// h - height of the rect
+		// xo - x coordinate of the origin of the rect
+		// yo - y coordinate of the origin of the rect
+		// xc - x coordinate of the corner opposite of the origin
+		// yc - y coordinate of the corner opposite of the origin
 		int w, h, xo, yo, xc, yc;
 
-		w = rand.nextInt(3 - 1 + 1) + 1;
-		h = rand.nextInt(3 - 1 + 1) + 1;
+		w = rand.nextInt(3 - 1 + 1) + 1; 
+		h = rand.nextInt(3 - 1 + 1) + 1; 
 
 		xo = rand.nextInt((gridSize - 1) - w + 1);
 		yo = rand.nextInt((gridSize - 1) - h + 1);
@@ -107,8 +104,8 @@ public class Map {
 		xc = xo + w;
 		yc = yo + h;
 
-		for (int i = xo; i < xo + w; i++) {
-			for (int j = yo; j < yo + h; j++) {
+		for (int i = yo; i < yo + h; i++) {
+			for (int j = xo; j < xo + w; j++) {
 				grid[i][j].setFloor(true);
 			}
 		}
@@ -123,7 +120,7 @@ public class Map {
 		int higher = Math.max(x1, x2);
 
 		for (int i = lower; i <= higher; i++) {
-			grid[i][y].setFloor(true);
+			grid[y][i].setFloor(true);
 		}
 	}
 	// Generates a line of floor tiles from the given two y coordinates at the given x coordinate
@@ -132,7 +129,7 @@ public class Map {
 		int higher = Math.max(y1, y2);
 
 		for (int i = lower; i <= higher; i++) {
-			grid[x][i].setFloor(true);
+			grid[i][x].setFloor(true);
 		}
 	}
 	
