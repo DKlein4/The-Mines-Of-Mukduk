811fb91dc9cc409337eb02f0f6094a40825f132f
Dustin <Dustin@Dustins-Laptop>
2017-09-28 01:15:53 -0500

    Added a sprite for the player

--- The Mines of Mukduk/src/entities/Player.java
+++ The Mines of Mukduk/src/entities/Player.java
@@ -1,21 +1,33 @@
 package entities;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 
-import Main.main;
-import user_interface.GUIMain;
+import javax.imageio.ImageIO;
+
 import user_interface.Handler;
 
 public class Player extends Entity {
 	Random rand = new Random();
 	Handler handler;
+	File pathToSprite;
+    Image playerSprite;
+
 
 	public Player(int row, int col, ID id, Handler handler) {
 		super(row, col, ID.Player);
 		this.handler = handler;
+		
+		try {
+			pathToSprite = new File("src/Resources/Dwarf.png");
+			playerSprite = ImageIO.read(pathToSprite);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void tick() {
@@ -26,8 +38,7 @@ public class Player extends Entity {
 
 	public void render(Graphics g) {
 		// Draws the player on the GUI. Right now its just a white square
-		g.setColor(Color.white);
-		g.fillRect(xPos, yPos, tileSizeY, tileSizeY);
+		g.drawImage(playerSprite, xPos, yPos, tileSizeY, tileSizeY, null);
 	}
 
 	public Rectangle getBounds() {
